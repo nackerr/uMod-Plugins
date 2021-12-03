f1dff36a15ade94e8e5c0bd68ff634c3d999b5ec
Ryan Nacker <nackerr@rider.edu>
2021-12-03 15:02:24 -0500

    Update AntiHackHotfix.cs
    Added permissions

--- Plugins/AntiHackHotfix.cs
+++ Plugins/AntiHackHotfix.cs
@@ -1,11 +1,16 @@
 namespace Oxide.Plugins
 {
-    [Info("AntiHack Hotfix", "Ryz0r", "1.0.0"), Description("Temporary fix for the admin in rock violation.")]
+    [Info("AntiHack Hotfix", "Ryz0r", "1.0.1"), Description("Temporary fix for the admin in rock violation.")]
     public class AntiHackHotfix : RustPlugin
     {
+        private void Init()
+        {
+            permission.RegisterPermission("ahhf.bypass", this);
+        }
+        
         private object OnPlayerViolation(BasePlayer player, AntiHackType type, float amount)
         {
-            if (type == AntiHackType.InsideTerrain && player.IsAdmin) return false;
+            if (type == AntiHackType.InsideTerrain && (player.IsAdmin || permission.UserHasPermission(player.UserIDString, "ahhf.bypass"))) return false;
             return null;
         }
     }
